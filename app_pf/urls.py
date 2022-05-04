890f5fec3d99c427b09bdda4953389783175b2a4
labourdigitalrpa <salvador@labour.digital>
2022-05-04 19:43:07 -0300

    Inserção de dados no banco foi criada com o metodo cadastrarClientePf

--- app_pf/urls.py
+++ app_pf/urls.py
@@ -7,4 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('fomrulario_pf/', views.formulario_pf, name='formulario_pf'),
+    path('cadastrarClientePf/',views.cadastrarClientePf,name='cadastrarClientePf'),
     ]
--- app_pf/views.py
+++ app_pf/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app_pf.forms import clientePfForm
 
 def index(request):
@@ -8,3 +8,10 @@ def formulario_pf(request):
     data = {}
     data['form'] = clientePfForm()
     return render(request, 'formulario_pf.html', data)
+
+def cadastrarClientePf(request):
+    form = clientePfForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('index')
+
